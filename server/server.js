26c7e63a600494a442a9240d3f8468a9adf182a8
Bilash Prosad <pbilash64@gmail.com>
2023-08-23 02:33:24 +0600

    database added, custom error handler created

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 import "dotenv/config";
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { connectDatabase } from "./config/database.js";
 import { authRoutes } from "./routes/authRoutes.js";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 
 const app = express();
 
+connectDatabase();
+
+app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/v1", authRoutes);
 
-const port = process.env.port || 4000;
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
